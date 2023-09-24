c24f18edd235a1df13e30477af2ebc4177462e6c
yhibare <96299060+yashhibare7@users.noreply.github.com>
2023-09-24 22:32:57 +0530

    init

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import  ReactDOM  from "react-dom";
 
 const name = "Yash";
+const currentDate = new Date();
+const year = currentDate.getFullYear();
 
 ReactDOM.render(
   <div>
-    <h1>Hello {name}</h1>
+    <p>Hello {name}</p>
+    <p>Copyright {year}</p>
   </div>,
   document.getElementById("root")
 );
