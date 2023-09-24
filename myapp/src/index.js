2e9e3d50b1d9d80f94a3049f51a328bb215007d4
yhibare <96299060+yashhibare7@users.noreply.github.com>
2023-09-24 23:02:42 +0530

    init

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,14 +1,23 @@
 import React from "react";
 import  ReactDOM  from "react-dom";
+import "./styles.css";
 
 const name = "Yash";
 const currentDate = new Date();
 const year = currentDate.getFullYear();
 
+const img = "https://img.freepik.com/premium-photo/abstract-rainbow-colorful-bright-feather-closeup-up-macro-view-background-plumage-texture-with-dew-drops_753134-644.jpg";
+const img1 = "https://wallpapers.com/images/featured/hd-a5u9zq0a0ymy2dug.jpg";
+
 ReactDOM.render(
-  <div>
-    <p>Hello {name}</p>
-    <p>Copyright {year}</p>
+  <div className="heading">
+    <h1>Hello {name}</h1>
+    <li>Copyright {year}</li>
+    <div>
+      <img className="hd_img" src={img}/>
+      <img className="hd_img" src={img1}/>
+      <img className="hd_img" src={img1}/>
+    </div>
   </div>,
   document.getElementById("root")
 );
