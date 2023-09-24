f4bbe02d988e400809723d6d1a670a382a670713
yhibare <96299060+yashhibare7@users.noreply.github.com>
2023-09-24 22:08:47 +0530

    init

---
+++ myapp/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import  ReactDOM  from "react-dom";
+
+const name = "Yash";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello {name}</h1>
+  </div>,
+  document.getElementById("root")
+);
